794ad82845ba3e802475fe4e3170ae023fc8e4e6
jonathan <koren@pukogames.com>
2022-09-12 23:50:08 +0900

    Add rotation controls

--- FirstScript.gd
+++ FirstScript.gd
@@ -2,12 +2,30 @@ extends MeshInstance
 
 
 # Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var initialRotation = Vector3.ZERO
+var rotationVelocity = Vector3.ZERO
 
 
 # Called when the node enters the scene tree for the first time.
-#func _ready():
-#	pass # Replace with function body.
+func _ready():
+	initialRotation = self.rotation
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var direction = Vector3.ZERO
 	
+	if Input.is_key_pressed(KEY_RIGHT):
+		rotationVelocity.y += 0.1 * delta
+	if Input.is_key_pressed(KEY_LEFT):
+		rotationVelocity.y -= 0.1 * delta
+	if Input.is_key_pressed(KEY_UP):
+		rotationVelocity.x -= 0.1 * delta
+	if Input.is_key_pressed(KEY_DOWN):
+		rotationVelocity.x += 0.1 * delta
+	
+	self.rotate(Vector3.FORWARD, rotationVelocity.y)
+	self.rotate(Vector3.RIGHT, rotationVelocity.x)
+	
+	if Input.is_key_pressed(KEY_R):
+		self.rotation = initialRotation
+		rotationVelocity = Vector3.ZERO
