b60015fb1f56c52bd165ecf35c1debef767a4255
jonathan <koren@pukogames.com>
2022-09-13 00:57:40 +0900

    Use physics to apply rotation

--- scripts/FirstScript.gd
+++ scripts/FirstScript.gd
@@ -3,34 +3,36 @@ extends MeshInstance
 
 # Declare member variables here. Examples:
 var initialRotation = Vector3.ZERO
-var rotationVelocity = Vector3.ZERO
-
+var initialTranslation = Vector3.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	initialRotation = self.rotation
+	initialRotation = rigidBody().rotation
+	initialTranslation = rigidBody().translation
+
+func rigidBody():
+	return self.get_parent()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 
 	if Input.is_key_pressed(KEY_RIGHT):
-		rotationVelocity.y += 0.1 * delta
+		rigidBody().add_torque(Vector3.FORWARD * 15 * delta)
 	if Input.is_key_pressed(KEY_LEFT):
-		rotationVelocity.y -= 0.1 * delta
+		rigidBody().add_torque(Vector3.BACK * 15 * delta)
 	if Input.is_key_pressed(KEY_UP):
-		rotationVelocity.x -= 0.1 * delta
+		rigidBody().add_torque(Vector3.LEFT * 15 * delta)
 	if Input.is_key_pressed(KEY_DOWN):
-		rotationVelocity.x += 0.1 * delta
-	
-	self.rotate(Vector3.FORWARD, rotationVelocity.y)
-	self.rotate(Vector3.RIGHT, rotationVelocity.x)
+		rigidBody().add_torque(Vector3.RIGHT * 15 * delta)
+
 
 func _input(event):
 	if event is InputEventKey and event.pressed:
 		if event.scancode == KEY_ESCAPE:
 			self.get_tree().quit()
 		elif event.scancode == KEY_R:
-			self.rotation = initialRotation
-			rotationVelocity = Vector3.ZERO
+			rigidBody().rotation = initialRotation
+			rigidBody().translation = initialTranslation
+			rigidBody().gravity_scale = 0.0
 		elif event.scancode == KEY_SPACE:
-			self.get_parent().gravity_scale = 1.0
+			rigidBody().gravity_scale = 1.0
